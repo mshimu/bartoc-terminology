537ac272a2d7d7aea8943ba4d52605e82054eb40
Akter <Akter@16238-005.vzg.local>
2016-05-24 08:33:44 +0200

    Some update for the website framework (on going ...).

--- src/bartoc/terminology/de/BartocTerminology.java
+++ src/bartoc/terminology/de/BartocTerminology.java
@@ -6,10 +6,14 @@ import javax.annotation.ManagedBean;
 import com.mongodb.BasicDBList;
 
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class BartocTerminology.
+ */
 public class BartocTerminology {
-	
 //	private String classification;
 	private String prefLabel;
+	
 	private String altLabel;
 	private String scopeNote;  
 	private String creator;
@@ -22,57 +26,194 @@ public class BartocTerminology {
 //	private String[] ddc_notation;
 	private BasicDBList[] ddc;
 		
+/**
+ * Gets the ddc.
+ *
+ * @return the ddc
+ */
 public BasicDBList[] getDdc() {
 		return ddc;
 	}
+	
+	/**
+	 * Sets the ddc.
+	 *
+	 * @param ddc the new ddc
+	 */
 	public void setDdc(BasicDBList[] ddc) {
 		this.ddc = ddc;
 	}
+	
+	/**
+	 * Gets the pref label.
+	 *
+	 * @return the pref label
+	 */
 	//	public String getClassification() { return classification; }
 	public String getPrefLabel() { return prefLabel; }
+	
+	/**
+	 * Gets the alt label.
+	 *
+	 * @return the alt label
+	 */
 	public String getAltLabel() { return altLabel; }
+	
+	/**
+	 * Gets the scope note.
+	 *
+	 * @return the scope note
+	 */
 	public String getScopeNote() { return scopeNote; }
+	
+	/**
+	 * Gets the creator.
+	 *
+	 * @return the creator
+	 */
 	public String getCreator() { return creator; }
+	
+	/**
+	 * Gets the type.
+	 *
+	 * @return the type
+	 */
 	public String getType() { return type; }
+	
+	/**
+	 * Gets the wikipedia.
+	 *
+	 * @return the wikipedia
+	 */
 	public String getWikipedia() { return wikipedia; }
+	
+	/**
+	 * Gets the url.
+	 *
+	 * @return the url
+	 */
 	public String getUrl() { return url; }
+	
+	/**
+	 * Gets the subject.
+	 *
+	 * @return the subject
+	 */
 	public String getSubject() { return subject; }
+	
+	/**
+	 * Gets the classes.
+	 *
+	 * @return the classes
+	 */
 	public String getClasses() { return classes; }
+	
+	/**
+	 * Gets the ddc_notation.
+	 *
+	 * @return the ddc_notation
+	 */
 	public String getDdc_notation() { return ddc_notation; }
 	
 //	public void setClassification(String classification){
 //		this.classification = classification;
+/**
+ * Sets the pref label.
+ *
+ * @param prefLabel the new pref label
+ */
 //	}
 	public void setPrefLabel(String prefLabel){
 		this.prefLabel = prefLabel;
 	}
+	
+	/**
+	 * Sets the alt label.
+	 *
+	 * @param altLabel the new alt label
+	 */
 	public void setAltLabel(String altLabel){
 		this.altLabel = altLabel;
 	}
+	
+	/**
+	 * Sets the scope note.
+	 *
+	 * @param scopeNote the new scope note
+	 */
 	public void setScopeNote(String scopeNote){
 		this.scopeNote = scopeNote;
 	}
+	
+	/**
+	 * Sets the creator.
+	 *
+	 * @param creator the new creator
+	 */
 	public void setCreator(String creator){
 		this.creator = creator;
 	}
+	
+	/**
+	 * Sets the type.
+	 *
+	 * @param type the new type
+	 */
 	public void setType(String type){
 		this.type = type;
 	}
+	
+	/**
+	 * Sets the wikipedia.
+	 *
+	 * @param wikipedia the new wikipedia
+	 */
 	public void setWikipedia(String wikipedia){
 		this.wikipedia = wikipedia;
 	}
+	
+	/**
+	 * Sets the url.
+	 *
+	 * @param url the new url
+	 */
 	public void setUrl(String url){
 		this.url = url;
 	}
+	
+	/**
+	 * Sets the subject.
+	 *
+	 * @param subject the new subject
+	 */
 	public void setSubject(String subject){
 		this.subject = subject;
 	}
+	
+	/**
+	 * Sets the classes.
+	 *
+	 * @param classes the new classes
+	 */
 	public void setClasses(String classes){
 		this.classes = classes;
 	}
+	
+	/**
+	 * Sets the ddc_notation.
+	 *
+	 * @param ddc_notation the new ddc_notation
+	 */
 	public void setDdc_notation(String ddc_notation){
 		this.ddc_notation = ddc_notation;
 	}
+	
+	/**
+	 * Adds the.
+	 *
+	 * @param ddcnotation the ddcnotation
+	 * @return the object
+	 */
 	public Object add(String ddcnotation) {
 		// TODO Auto-generated method stub
 		return ddcnotation;
--- src/bartoc/terminology/de/ShowBartocTerminology.java
+++ src/bartoc/terminology/de/ShowBartocTerminology.java
@@ -1,9 +1,19 @@
+/**
+ * This file is part of a Web Application Software for viewing the 
+ * Bartoc Terminologies on the web. 
+ * 
+ * getTerminology, resetData, fileDownload are the methods under the 
+ * class named ShowBartocTerminology.
+ */
+
 package bartoc.terminology.de;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ApplicationScoped;
@@ -13,14 +23,20 @@ import bartoc.terminology.de.BartocTerminology;
 import javax.annotation.PostConstruct;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;  
+import javax.json.stream.*;
 import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
 import javax.json.JsonWriter;  
+import javax.json.JsonWriterFactory;
+
 import org.primefaces.json.JSONException;
-import org.primefaces.model.StreamedContent;
 
 import com.mongodb.BasicDBList;
 import com.mongodb.BasicDBObject;
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.CommandResult;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
@@ -37,45 +53,121 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.IndexedColors;
 
+// TODO: Auto-generated Javadoc
+/**
+ * This class is the main ManagedBean which is operates all the methods 
+ * and accessing the database. 
+ * 
+ * <p>The getTerminology method usually connects with the mongodb and also 
+ * responsible to generate the query 
+ * and returns all the required query data as a list.
+ * Calling this method from another class is easy.   
+ * 
+ * <p>The resetData is a method where all the required queries are written 
+ * and it also setting the required field for the specific data table.
+ * 
+ * <p>The method fileDownload gives the flexibility to download the specific 
+ * data table as a JSON/JSKOS format. It calls the getTerminology method and 
+ * takes the data from the database and converts it into JSON/JSKOS format.
+ *  
+ * @author M. Akter 
+ * @version $1.0 $ 
+ *  
+ */
+
 
 @ManagedBean(name = "showTerminology")  
 @ApplicationScoped
 public class ShowBartocTerminology {
+	
+	/** The table. */
 	DBCollection table = null;
+	
+	/** The db. */
 	DB db = null;
+	
+	/** The mongo. */
 	Mongo mongo = null;
+	
+	/** The cursor. */
 	DBCursor cursor = null;
+	
+	/** The query. */
 	private DBObject query=null;
+	
+	/** The fields. */
 	private BasicDBObject fields=null;
+	
+	/** The list. */
 	private List<BartocTerminology> list = new ArrayList<BartocTerminology>();
+	
+	/** The lst. */
 	private List<BartocTerminology> lst;
+	
+	/** The filtered terminologies. */
 	private List<BartocTerminology> filteredTerminologies = null;
+	
+	/** The list name. */
 	private String listName;
 
+	/**
+	 * Gets the list name.
+	 *
+	 * @return the list name
+	 */
 	public String getListName() {
 		return listName;
 	}
 
+	/**
+	 * Sets the list name.
+	 *
+	 * @param listName the new list name
+	 */
 	public void setListName(String listName) {
 		this.listName = listName;
 	}
 	
+	/**
+	 * Gets the query.
+	 *
+	 * @return the query
+	 */
 	public DBObject getQuery() {
 		return query;
 	}
 
+	/**
+	 * Sets the query.
+	 *
+	 * @param query the new query
+	 */
 	public void setQuery(DBObject query) {
 		this.query = query;
 	}
 
+	/**
+	 * Gets the fields.
+	 *
+	 * @return the fields
+	 */
 	public BasicDBObject getFields() {
 		return fields;
 	}
 
+	/**
+	 * Sets the fields.
+	 *
+	 * @param fields the new fields
+	 */
 	public void setFields(BasicDBObject fields) {
 		this.fields = fields;
 	}
 	
+	/**
+	 * Reset data.
+	 */
+	//Queries for the data table
 	@PostConstruct
 	public void resetData() {
 		try{
@@ -110,12 +202,19 @@ public class ShowBartocTerminology {
 		
 	}
 
+	/**
+	 * Gets the terminology.
+	 *
+	 * @return the terminology
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 */
+	//Returns the data as a list
 	public List<BartocTerminology> getTerminology() throws IOException{
-//			mongo = new Mongo("localhost",27017);
-//			db = mongo.getDB("MongoDatabase");
-			mongo = new MongoClient(new ServerAddress("esx-128.gbv.de", 27017));
+			mongo = new Mongo("localhost",27017);
+			db = mongo.getDB("MongoDatabase");
+//			mongo = new MongoClient(new ServerAddress("esx-128.gbv.de", 27017));
                //     new MongoClientOptions.Builder().connectTimeout(300000).socketTimeout(300000).build());
-			db = mongo.getDB("BartocDatabase");
+//			db = mongo.getDB("BartocDatabase");
 			table = db.getCollection("data_bartoc");
 			list = new ArrayList<BartocTerminology>();
 			BartocTerminology bT = null;
@@ -125,7 +224,6 @@ public class ShowBartocTerminology {
 			DBCursor cursor = table.find(getQuery(), getFields());
 				while (cursor.hasNext()) {
 					DBObject obj = cursor.next();
-//					String ddcnotation = null;
 					ArrayList<DBObject> description = (ArrayList<DBObject>)obj.get("Description"); 
 						if(description!=null){
 							for(DBObject embedded : description){
@@ -138,13 +236,19 @@ public class ShowBartocTerminology {
 								bT.setWikipedia((String)embedded.get("wikipedia"));
 								bT.setUrl((String)embedded.get("url"));
 								bT.setSubject((String)embedded.get("subject"));
-								bT.setDdc_notation((String) embedded.get("ddc_notation"));
-//								BasicDBList Ddc = (BasicDBList) embedded.get("ddc_notation");
-//									for(Object dbObj : Ddc) {
-//				    			
-//										// shows each item from the ddc_notation array
-//										ddcnotation = dbObj.toString();
-//									}	
+							//	bT.setDdc_notation((String) embedded.get("ddc_notation"));
+								BasicDBList asList = null;
+								//BasicDBObject Ddc = (BasicDBObject) embedded.get("ddc_notation");
+								BasicDBList Ddc = (BasicDBList) embedded.get("ddc_notation");
+									for(Object dbObj : Ddc) {
+										asList = new BasicDBList();
+										asList.add(dbObj);
+//				    				
+										// shows each item from the ddc_notation array
+									//	ddcnotation = dbObj.toString();
+										
+									}	
+				    				bT.setDdc_notation(asList.toString());
 								bT.setClasses((String)embedded.get("classes"));
 								list.add(bT);
 							}
@@ -154,29 +258,51 @@ public class ShowBartocTerminology {
 		
 	}
 	
+	/**
+	 * Gets the filtered terminologies.
+	 *
+	 * @return the filtered terminologies
+	 */
 	public List<BartocTerminology> getFilteredTerminologies() {
 		return filteredTerminologies;
 	}
 
+	/**
+	 * Sets the filtered terminologies.
+	 *
+	 * @param filteredTerminologies the new filtered terminologies
+	 */
 	public void setFilteredTerminologies(
 			List<BartocTerminology> filteredTerminologies) {
 		this.filteredTerminologies = filteredTerminologies;
 	}
 	
-	public void postProcessXLS(Object document) {
-        HSSFWorkbook wb = (HSSFWorkbook) document;
-        HSSFSheet sheet = wb.getSheetAt(0);
-        CellStyle style = wb.createCellStyle();
-        style.setFillBackgroundColor(IndexedColors.AQUA.getIndex());
- 
-        for (org.apache.poi.ss.usermodel.Row row : sheet) {
-            for (Cell cell : row) {
-                cell.setCellValue(cell.getStringCellValue().toUpperCase());
-                cell.setCellStyle(style);
-            }
-        }
-    }
+	/**
+	 * Post process xls.
+	 *
+	 * @param document the document
+	 */
+//	public void postProcessXLS(Object document) {
+//        HSSFWorkbook wb = (HSSFWorkbook) document;
+//        HSSFSheet sheet = wb.getSheetAt(0);
+//        CellStyle style = wb.createCellStyle();
+//        style.setFillBackgroundColor(IndexedColors.AQUA.getIndex());
+// 
+//        for (org.apache.poi.ss.usermodel.Row row : sheet) {
+//            for (Cell cell : row) {
+//                cell.setCellValue(cell.getStringCellValue().toUpperCase());
+//                cell.setCellStyle(style);
+//            }
+//        }
+//    }
 	
+	/**
+	 * File download.
+	 *
+	 * @throws IOException Signals that an I/O exception has occurred.
+	 * @throws JSONException the JSON exception
+	 */
+	//Download the data from the database
 	public void fileDownload() throws IOException, JSONException{
 		String fileName = "dataTable";
 		FacesContext fc = FacesContext.getCurrentInstance();
@@ -187,42 +313,86 @@ public class ShowBartocTerminology {
 	    OutputStream wr = ec.getResponseOutputStream();
 //	    InputStream stream = FacesContext.getCurrentInstance().getExternalContext().getResourceAsStream("D:/Software/Workplace/bartoc-terminology/WebContent/resources/dataTable.JSON");
 	    fc.responseComplete();
-		JsonWriter writer = null;
+	    JsonWriter writer = null;
 		lst = getTerminology();
+		JsonArrayBuilder builder = Json.createArrayBuilder();
 		for(BartocTerminology dataTable:lst){
-			JsonObject btocObject = (JsonObject) Json.createObjectBuilder()
-				.add("Subject", Json.createArrayBuilder()
-					.add(Json.createObjectBuilder().add("prefLabel", dataTable.getPrefLabel())
+			builder.add(Json.createObjectBuilder()
+					.add("subject", Json.createArrayBuilder()
+						.add(Json.createObjectBuilder()
+							.add("inScheme", Json.createArrayBuilder()
+								.add(Json.createObjectBuilder().add("uri", "http://dewey.info/scheme/")
+								.build())
+							.build())	
+							.add("notation",Json.createArrayBuilder()
+								.add(dataTable.getDdc_notation().toString())
+			            	.build())
+			            .build())
+			            .add(Json.createObjectBuilder()
+			            	.add("prefLable", Json.createObjectBuilder().add("en", dataTable.getSubject())
+			            		.build())
+			            .build())
+			        .build()) 
+			        .add("CLASSES", dataTable.getClasses())
+			        .add("notation", Json.createArrayBuilder()
+			        		.add(dataTable.getAltLabel())
 					.build())
-		            .add(Json.createObjectBuilder().add("notation",Json.createArrayBuilder()
-		            	.add(dataTable.getDdc_notation().toString())
-		            	.build())
-		            .build())	
-    				.add(Json.createObjectBuilder().add("inScheme", Json.createArrayBuilder()
-    					 .add(Json.createObjectBuilder().add("altLabel", dataTable.getAltLabel())
-		                 .add("url", dataTable.getUrl()).add("identifier", dataTable.getWikipedia())
-    					 	.build())
-    					 .build())
-    				.build())	 
-		            .add(Json.createObjectBuilder().add("scopeNote", dataTable.getScopeNote()))
-		            .add(Json.createObjectBuilder().add("type", dataTable.getType()))
-		            .add(Json.createObjectBuilder().add("creator", dataTable.getCreator()))
-		            .add(Json.createObjectBuilder().add("subject", dataTable.getSubject()))
-		            .add(Json.createObjectBuilder().add("classes", dataTable.getClasses()))
-		        .build())
-		    .build();
-			writer = Json.createWriter(wr);
-		    writer.writeObject(btocObject); 
+					.add("scopeNote", Json.createArrayBuilder()
+			        		.add(dataTable.getScopeNote())
+			        .build())
+			        .add("type", Json.createArrayBuilder()
+			        		.add("http://www.w3.org/2004/o2/skos/core#ConceptScheme")
+			        .build())		
+					.add("TYPE", dataTable.getType())
+					.add("url", dataTable.getUrl())
+					.add("creator", Json.createArrayBuilder()
+				    	.add(Json.createObjectBuilder().add("prefLabel", Json.createObjectBuilder()
+				    		.add("de", dataTable.getCreator())
+				    		.build())
+				    	.build())
+				    .build())
+				    .add("prefLabel", Json.createObjectBuilder()
+				    	.add("de", dataTable.getPrefLabel())
+			        .build())
+			        .add("identifier", Json.createArrayBuilder()
+			        	.add(dataTable.getWikipedia())
+			        .build())
+			    .build());
 		}
+		Map<String, Object> termProperties = new HashMap<>(1);
+		termProperties.put(JsonGenerator.PRETTY_PRINTING, true);
+		JsonArray btocObject = builder.build();
+		JsonWriterFactory writerFactory = Json.createWriterFactory(termProperties);
+		writer = writerFactory.createWriter(wr);
+		writer.writeArray(btocObject);
 		writer.close();
 	    wr.close();
 	}
 	
+	/**
+	 * Xml download.
+	 */
 	public void xmlDownload(){
 		
 	}
 	
+	/**
+	 * Search term.
+	 */
 	public void searchTerm(){
+		DBObject command = BasicDBObjectBuilder
+                .start("text", "data_bartoc")
+                .append("search", "altLabel")
+                .get();
+
+		CommandResult result =  db.command(command);
+		BasicDBList results = (BasicDBList) result.get("Description");
+
+		for(Object o : results) {
+			DBObject dbo = (DBObject) ((DBObject) o).get("obj");
+			String id = (String) dbo.get("_ID");
+			System.out.println(id);
+		}
 		
 	}
 
@@ -232,8 +402,4 @@ public class ShowBartocTerminology {
 //		col.getTerminology();
 //		col.fileDownload();
 //	}
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 256f76728492faa6e586cb407c17c49fab529953
